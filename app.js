a29e8227be436506e26e53de2d3b3eb1314b3f27
Guy Farley <guy.e.farley@gmail.com>
2022-03-24 15:55:29 -0700

    added app.js and tweaked style.css slightly

---
+++ app.js
@@ -0,0 +1,26 @@
+
+function getName() {
+
+    let name = prompt("What is your name?");
+    document.write("Welcome to my page, " + name + "!");
+
+}
+
+function getStream() {
+
+    let streaming = prompt("Which streaming service do you typically use to watch TV shows?");
+    document.write("Ok, " + streaming + " is a very reputable streaming service. Nice work.");
+
+}
+
+function getShow() {
+
+    let show = prompt("What is your favorit show?");
+    if (show === "Breaking Bad") {
+        document.write("Great choice! Breaking Bad is the greatest show of all time.");
+    } else if (show === "breaking bad") {
+        document.write("Great choice! Breaking Bad is the greatest show of all time.");
+    } else {
+        document.write("WRONG! " + show + " is a great show but Breaking Bad is the greatest show of all time. Thanks for playing.");
+    }
+}
