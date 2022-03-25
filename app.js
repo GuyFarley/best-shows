fa07eda604a0bb766a903b10eff9430d5c940045
Guy Farley <guy.e.farley@gmail.com>
2022-03-24 17:03:43 -0700

    added loops into app.js, updated index.html and style.css slightly

--- app.js
+++ app.js
@@ -24,3 +24,28 @@ function getShow() {
         document.write("WRONG! " + show + " is a great show but Breaking Bad is the greatest show of all time. Thanks for playing.");
     }
 }
+
+function rateBach() {
+    let answer;
+    let guesses = 0;
+
+    do {
+        guesses++;
+        answer = prompt("On a scale of 1-10 (with 10 being the highest), what would you rate the show The Bachelor?");
+    } while (parseInt(answer) !== 1)
+
+    if (parseInt(answer) === 1) {
+        document.write("Regarding The Bachelor, " + answer + " out of 10 is correct. The Bachelor is a bad show. It took you " + guesses + " times to rate it correctly.")
+    }
+}
+
+function tvHours() {
+    let output = '';
+    let tvHours = prompt("How many hours of TV do you watch per week?");
+
+    for (let i = 0; i < tvHours; i++) {
+        output += "<img src='images/TV.png' />";
+    }
+    document.write(output);
+    document.write("  Wow, that's a lot of TV! You should maybe crack a book sometime.")
+}
